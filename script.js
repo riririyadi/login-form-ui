48ffd50085199b50f968c4aa7895af33f7903c95
riririyadi <riririyadi@github.com>
2021-05-01 22:59:27 +0700

    added toggle password visibility

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function removeBoxShadow(id) {
   const element = document.getElementById(id);
   element.style.boxShadow = "";
 }
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#passwordField');
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
